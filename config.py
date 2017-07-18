85ca028b7111385441deee816e085b5a0afb8ada
NyleVII <stephenantymis@gmail.com>
2017-07-17 20:28:14 -0600

    Initial commit

---
+++ config.py
@@ -0,0 +1,8 @@
+''' AUTHORS: Stephen Antymis
+	VERSION: 0.1
+	DESCRIPTION: This is the config file. Please fill out your variables as indicated.
+'''
+
+ACCOUNT = "Nyle" #Your account name
+LEAGUE = "Standard" #What league do you want the program to use?
+TABNAME = "QCR" #This should be the name of the tab you're using to grab items out of
---
+++ qcr.py
@@ -0,0 +1,5 @@
+''' AUTHORS: Stephen Antymis
+VERSION: 0.1
+DESCRIPTION: Quick Chaos Recipe is designed as a tool for Path of Exile to quickly find chaos recipe items in a specific stash tab and click them in to your inventory. Due to macro
+				limitations of one action per click, this tool will have to cycle equipment slots per button press in accordance to PoE's ToS 
+'''
